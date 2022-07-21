0125114e090a473bdb3df33f8faaf0ef8b5a3474
Abhishek Mishra <abmishra700@gmail.com>
2022-07-21 18:11:28 +0530

    Pagination (Goto Page)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { BasicTable } from "./components/BasicTable";
-import { SortingTable } from "./components/SortingTable";
-import { FilteringTable } from "./components/filteringTable";
+// import { BasicTable } from "./components/BasicTable";
+// import { SortingTable } from "./components/SortingTable";
+// import { FilteringTable } from "./components/filteringTable";
+import { PaginationTable } from "./components/PaginationTable";
 
 function App() {
   return (
     <div className="App">
       {/* <BasicTable /> */}
       {/* <SortingTable /> */}
-      <FilteringTable />
+      {/* <FilteringTable /> */}
+      <PaginationTable />
     </div>
   );
 }
---
+++ src/components/PaginationTable.js
@@ -0,0 +1,75 @@
+import React, { useMemo } from "react";
+import { useTable, usePagination } from "react-table";
+import MOCK_DATA from "../MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+
+export const PaginationTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    nextPage,
+    previousPage,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    state,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    usePagination
+  );
+
+  const { pageIndex, pageSize } = state;
+
+  return (
+    <>
+      <table {...getTableProps()} id="customers">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous Page
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next Page
+        </button>
+      </div>
+    </>
+  );
+};
