b2bc3db0755be3affe69c6ba2952f270177d3a14
Abhishek Mishra <abmishra700@gmail.com>
2022-07-21 16:40:41 +0530

    sorting added

---
+++ src/components/columns.js
@@ -0,0 +1,77 @@
+export const COLUMNS = [
+  {
+    Header: "ID",
+    footer: "ID",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    footer: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    footer: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "Date of Birth",
+    footer: "Date of Birth",
+    accessor: "date_of_birth",
+  },
+  {
+    Header: "Country",
+    footer: "Country",
+    accessor: "country",
+  },
+  {
+    Header: "Phone",
+    footer: "Phone",
+    accessor: "phone",
+  },
+];
+
+export const GROUPS_COLUMNS = [
+  {
+    Header: "ID",
+    footer: "ID",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    footer: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        footer: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        footer: "Last Name",
+        accessor: "last_name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    footer: "Info",
+    columns: [
+      {
+        Header: "Date of Birth",
+        footer: "Date of Birth",
+        accessor: "date_of_birth",
+      },
+      {
+        Header: "Country",
+        footer: "Country",
+        accessor: "country",
+      },
+      {
+        Header: "Phone",
+        footer: "Phone",
+        accessor: "phone",
+      },
+    ],
+  },
+];
