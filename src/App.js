011fbfbe601dbb9f279ddc121d2d0c8ea756ffc3
Abhishek Mishra <abmishra700@gmail.com>
2022-07-25 12:42:37 +0530

    tanstack/react-table": "8.0.0-beta.4 table

--- src/App.js
+++ src/App.js
@@ -1,121 +1,123 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { Container } from "semantic-ui-react";
+import V8table from "./components/V8-table";
+// import { Container } from "semantic-ui-react";
 // import { BasicTable } from "./components/BasicTable";
 // import { SortingTable } from "./components/SortingTable";
 // import { FilteringTable } from "./components/filteringTable";
 // import { PaginationTable } from "./components/PaginationTable";
 import TableContainer from "./TableContainer";
-import { SelectColumnFilter } from "./filters";
-import { Button } from "@mui/material";
+// import { SelectColumnFilter } from "./filters";
+// import { Button } from "@mui/material";
 
 function App() {
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Title",
-        accessor: "name.title",
-        disableSortBy: true,
-        disableFilters: true,
-        width: 100,
-      },
-      {
-        Header: "Age",
-        accessor: "registered.age",
-        width: 100,
-        Footer: (info) => {
-          const total = React.useMemo(
-            () =>
-              info.rows.reduce(
-                (sum, row) => row.original.registered.age + sum,
-                0
-              ),
-            [info.rows]
-          );
-          return (
-            <>
-              <b>Total</b>: {total}
-            </>
-          );
-        },
-      },
-      {
-        Header: "First Name",
-        accessor: "name.first",
-        width: 150,
-      },
-      {
-        Header: "Last Name",
-        accessor: "name.last",
-        width: 150,
-      },
-      {
-        Header: "Email",
-        accessor: "email",
-        width: 300,
-      },
-      {
-        Header: "City",
-        accessor: "location.city",
-        width: 150,
-      },
-      {
-        Header: "State",
-        accessor: "location.state",
-        width: 200,
-      },
-      {
-        Header: "Post Code",
-        accessor: "location.postcode",
-        width: 100,
-      },
-      {
-        Header: "Action",
-        accessor: "",
-        width: 100,
-        disableSortBy: true,
-        disableFilters: true,
-        Cell: (info) => {
-          return (
-            <Button
-              onClick={() => {
-                // console.log(info.row.original);
-                handleEdit(info.row.original);
-              }}
-            >
-              Edit
-            </Button>
-          );
-        },
-      },
-    ],
-    []
-  );
+  // const columns = useMemo(
+  //   () => [
+  //     {
+  //       Header: "Title",
+  //       accessor: "name.title",
+  //       disableSortBy: true,
+  //       disableFilters: true,
+  //       width: 100,
+  //     },
+  //     {
+  //       Header: "Age",
+  //       accessor: "registered.age",
+  //       width: 100,
+  //       Footer: (info) => {
+  //         const total = React.useMemo(
+  //           () =>
+  //             info.rows.reduce(
+  //               (sum, row) => row.original.registered.age + sum,
+  //               0
+  //             ),
+  //           [info.rows]
+  //         );
+  //         return (
+  //           <>
+  //             <b>Total</b>: {total}
+  //           </>
+  //         );
+  //       },
+  //     },
+  //     {
+  //       Header: "First Name",
+  //       accessor: "name.first",
+  //       width: 150,
+  //     },
+  //     {
+  //       Header: "Last Name",
+  //       accessor: "name.last",
+  //       width: 150,
+  //     },
+  //     {
+  //       Header: "Email",
+  //       accessor: "email",
+  //       width: 300,
+  //     },
+  //     {
+  //       Header: "City",
+  //       accessor: "location.city",
+  //       width: 150,
+  //     },
+  //     {
+  //       Header: "State",
+  //       accessor: "location.state",
+  //       width: 200,
+  //     },
+  //     {
+  //       Header: "Post Code",
+  //       accessor: "location.postcode",
+  //       width: 100,
+  //     },
+  //     {
+  //       Header: "Action",
+  //       accessor: "",
+  //       width: 100,
+  //       disableSortBy: true,
+  //       disableFilters: true,
+  //       Cell: (info) => {
+  //         return (
+  //           <Button
+  //             onClick={() => {
+  //               // console.log(info.row.original);
+  //               handleEdit(info.row.original);
+  //             }}
+  //           >
+  //             Edit
+  //           </Button>
+  //         );
+  //       },
+  //     },
+  //   ],
+  //   []
+  // );
 
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const doFetch = async () => {
-      const response = await fetch("https://randomuser.me/api/?results=100");
-      const body = await response.json();
-      const contacts = body.results;
-      setData(contacts);
-    };
+  // useEffect(() => {
+  //   const doFetch = async () => {
+  //     const response = await fetch("https://randomuser.me/api/?results=100");
+  //     const body = await response.json();
+  //     const contacts = body.results;
+  //     setData(contacts);
+  //   };
 
-    doFetch();
-  }, []);
+  //   doFetch();
+  // }, []);
 
-  const handleEdit = (data) => {
-    alert(JSON.stringify(data));
-  };
+  // const handleEdit = (data) => {
+  //   alert(JSON.stringify(data));
+  // };
 
   return (
-    <Container
-      style={{
-        marginTop: "2em",
-      }}
-    >
-      <TableContainer columns={columns} data={data} />
-    </Container>
+    // <Container
+    //   style={{
+    //     marginTop: "2em",
+    //   }}
+    // >
+    //   <TableContainer columns={columns} data={data} />
+    // </Container>
+    <V8table />
   );
 }
 
---
+++ src/components/V8-table.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import {
+  createTable,
+  getCoreRowModel,
+  useTableInstance,
+} from "@tanstack/react-table";
+import Student from "../student.json";
+import { format } from "date-fns";
+
+const table = createTable();
+console.log(table);
+const defaultData = [...Student];
+
+const defaultColumns = [
+  table.createDataColumn("name", {
+    id: "name",
+    header: "Full Name",
+    footer: "Full Name",
+    cell: (row) => row.getValue().toUpperCase(),
+  }),
+  table.createDataColumn("email", {
+    id: "email",
+    header: "Email Address",
+    footer: "Email Address",
+  }),
+  table.createDataColumn("phone", {
+    id: "phone",
+    header: "Phone Number",
+    footer: "Phone Number",
+  }),
+  table.createDataColumn("standard", {
+    id: "standard",
+    header: "Class",
+    footer: "Class",
+  }),
+  table.createDataColumn("section", {
+    id: "section",
+    header: "Section",
+    footer: "Section",
+  }),
+  table.createDataColumn("age", {
+    id: "age",
+    header: "Age",
+    footer: "Age",
+  }),
+  table.createDataColumn("date_of_birth", {
+    id: "date_of_birth",
+    header: "Date of Birth",
+    footer: "Date of Birth",
+    cell: (row) => format(new Date(row.getValue()), "dd/MM/yyyy"),
+  }),
+  table.createDataColumn("date_of_admission", {
+    id: "date_of_admission",
+    header: "Date of Admission",
+    footer: "Date of Admission",
+    cell: (row) => format(new Date(row.getValue()), "dd/MM/yyyy"),
+  }),
+  table.createDataColumn((row) => row.address.city, {
+    id: "city",
+    header: "City",
+    footer: "City",
+  }),
+  table.createDataColumn((row) => row.address.state, {
+    id: "state",
+    header: "State",
+    footer: "State",
+  }),
+  table.createDataColumn((row) => row.address.pincode, {
+    id: "pincode",
+    header: "Pincode",
+    footer: "Pincode",
+  }),
+  table.createDataColumn((row) => row.address.street, {
+    id: "street",
+    header: "Street",
+    footer: "Street",
+  }),
+  table.createDataColumn((row) => `${row.address.city}, ${row.address.state}`, {
+    id: "city_state",
+    header: "City State",
+    footer: "City State",
+  }),
+];
+
+export default function V8table() {
+  const [data, setData] = useState([...defaultData]);
+  const [columns, setColumns] = useState([...defaultColumns]);
+
+  const tableInstance = useTableInstance(table, {
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <table border={1}>
+        <thead>
+          {tableInstance.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder ? null : header.renderHeader()}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {tableInstance.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{cell.renderCell()}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+
+        <tfoot>
+          {tableInstance.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((footer) => (
+                <th key={footer.id}>
+                  {footer.isPlaceholder ? null : footer.renderFooter()}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
+  );
+}
